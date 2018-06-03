beb5c1b8dd900489416186aece71c79e3f2f3566
RudyKozik <kozikrudolf@gmail.com>
2018-06-03 17:41:55 +0200

    adding functions

--- zamiesanie/Source.cpp
+++ zamiesanie/Source.cpp
@@ -1,39 +1,51 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <time.h>
 
 using namespace std;
 
-void main()
+char loading()
 {
 	const int x = 2;
 	const int y = 7;
 	const int z = 5;
 
-	char myArray[x][y][z];
-
 	ifstream file("d:\\Projects\\file.txt");
+
+	char myArray[x][y][z];
 
 	for (int i = 0; i < x; i++)
 	{
 		for (int j = 0; j < y; j++)
 		{
-			for (int k = 0; k < z; k++)
-			{
-				file >> myArray[i][j][k];
-			}
+				file >> myArray[i][j];
 		}
 	}
 
-	for (int i = 0; i < x; ++i)
+	return myArray[x][y][z];
+}
+
+void writing(char myArray[2][7][5])
+{
+	for (int i = 0; i < 2; ++i)
 	{
-		for (int j = 0; j < y; ++j)
+		for (int j = 0; j < 7; ++j)
 		{
-			for (int k = 0; k < z; ++k)
-			{
-				cout << myArray[i][j][k];
-			}
+			cout << myArray[i][j];
 		}
 	}
+}
 
+int random()
+{
+	int random;
+	srand(time(NULL));
+	random = rand() % 7 - 1;
+	return random;
+}
+
+void main()
+{
 	cin.get();
 }
