14f94d47aafdab5cc7b3e32deea610bc715f73a6
RudyKozik <kozikrudolf@gmail.com>
2018-06-04 17:42:54 +0200

    a

--- zamiesanie/Source.cpp
+++ zamiesanie/Source.cpp
@@ -5,47 +5,72 @@
 
 using namespace std;
 
-char loading()
-{
-	const int x = 2;
-	const int y = 7;
-	const int z = 5;
-
-	ifstream file("d:\\Projects\\file.txt");
-
-	char myArray[x][y][z];
-
-	for (int i = 0; i < x; i++)
-	{
-		for (int j = 0; j < y; j++)
-		{
-				file >> myArray[i][j];
-		}
-	}
-
-	return myArray[x][y][z];
-}
-
-void writing(char myArray[2][7][5])
-{
-	for (int i = 0; i < 2; ++i)
-	{
-		for (int j = 0; j < 7; ++j)
-		{
-			cout << myArray[i][j];
-		}
-	}
-}
-
-int random()
-{
-	int random;
-	srand(time(NULL));
-	random = rand() % 7 - 1;
-	return random;
-}
-
 void main()
 {
+	srand(time(NULL));
+	string riadok;
+	ifstream infile;
+	string dvojica[5][2][12];
+	int poradie[5];
+	int random;
+	int skupina = 0;
+	int clen = 0;
+	int pismeno = 0;
+
+	infile.open("d:\\Projects\\file.txt");
+
+	//Nacitanie zo zlozky do 3D pola
+	while (infile.eof() == false)
+	{
+		getline(infile, riadok);
+
+		if (riadok == "")
+		{
+			skupina++;
+			clen = 0;
+			continue;
+		}
+
+		for (int i = 0; i < riadok.length(); i++)
+		{
+			dvojica[skupina][clen][pismeno] = riadok[i];
+			pismeno++;
+		}
+
+		pismeno = 0;
+		clen++;
+
+	}
+
+	//Generovanie poradia
+	for (int i = 0; i < 5; i++)
+	{
+		poradie[i] = -1;
+	}
+
+	for (int i = 0; i < 5; i++)
+	{
+		do
+		{
+			random = rand() %5;
+		} while (poradie[random] != -1);
+
+		poradie[random] = i;
+	}
+
+	//Vypis na obrazovku
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+			for (int k = 0; k < 12; k++)
+			{
+				cout << dvojica[poradie[i]][j][k];
+			}
+			cout << endl;
+		}
+		cout << endl;
+	}
+
 	cin.get();
 }
